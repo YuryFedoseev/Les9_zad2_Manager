170892e7f38534d8e7509c44aee518b942bd7b77
Yury.Fedoseev <Yri95@inbox.ru>
2021-07-23 23:53:33 +0300

    Первая версия

---
+++ src/main/java/PostManager.java
@@ -0,0 +1,15 @@
+public class PostManager {
+
+    private int privateID;
+    private String domain;
+    private String query;
+//добавить ограничение if then для ограничения символов
+    private boolean ownersOnly;
+    private byte count;
+    private int offset;
+    private boolean extended;
+    private String fields;
+
+
+
+}
